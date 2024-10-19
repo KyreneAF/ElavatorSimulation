b870e426c159b389af7016b208e621910903c144
Kyrene <Kyrene.Aflores@gmail.com>
2024-10-19 13:16:48 -0500

    finished button functionality and set up Elevator1 component

--- my-app/src/components/EastBuilding/EastElevator.jsx
+++ my-app/src/components/EastBuilding/EastElevator.jsx
@@ -3,6 +3,8 @@ import "./EastElevator.css";
 
 function EastElevator() {
   const [floorBttns, setFloorBttns] = useState([]);
+  // const [buttonStates, setButtonStates] = useState([]);
+  // const [buttonStates, setButtonStates] = useState(Array(24).fill("light")); // this fills a copy array of floors but containing the class name to change button color
   const [buttonStates, setButtonStates] = useState([]);
   const [floorQue, setFloorQue] = useState([]);
 
@@ -14,32 +16,39 @@ function EastElevator() {
       resArr.push(i);
     }
     setFloorBttns(resArr);
+    setButtonStates(Array(24).fill("light"));
   }, []);
 
-  //This function removes the floors from the que
-  const handleElevatorMotion = (newQue) => {
-    setTimeout(() => {
-      if (newQue && newQue.length > 0) {
-        const nextQue = [...newQue.slice(1)];
-        setFloorQue(nextQue);
-      }
-    }, 3000);
-  };
   //This Func adds the floor to the elevator que
-  const handleElevatorQue = (num) => {
-    const newQue = [...floorQue, num];
-    setFloorQue(newQue);
-    handleElevatorMotion(newQue);
+  const handleElevatorQue = (index, num) => {
+    const check = floorQue.find((n) => n == num); // Check if num alread in que
+    if (!check) {
+      const newQue = [...floorQue, num];
+      setFloorQue(newQue);
+      // handleElevatorMotion(index, newQue);
+    }
   };
-
   //THis Func handles when the floor button is clicked
   const handleClick = (index, num) => {
+    console.log("buttonstates first", buttonStates);
     const newButtonStates = [...buttonStates]; //spreads buttonStates into an array variable
     newButtonStates[index] = "success"; //changes the buttonState at the clicked index to success which turns it green
     setButtonStates(newButtonStates); //updates buttonStates to the newButtonStates array
-    handleElevatorQue(num);
-    handleElevatorMotion();
+    handleElevatorQue(index, num);
   };
+  //This function removes the floors from the que
+  // const handleElevatorMotion = (index, newQue) => {
+  //   setTimeout(() => {
+  //     if (newQue && newQue.length > 0) {
+  //       const indxOfNum = floorBttns.indexOf(newQue[0]); // find index of first num in que we cannot pass the index because that does not get updated
+  //       const nextQue = [...newQue.slice(1)]; // Here we are slicing the first number in our que and setting our floor que to the remainder que
+  //       setFloorQue(nextQue); //updating our que with the first value popped
+  //       const btnStateCopy = [...buttonStates];
+  //       btnStateCopy[indxOfNum] = "light";
+  //       setButtonStates(btnStateCopy);
+  //     }
+  //   }, 3000);
+  // };
 
   console.log("eleQue", floorQue);
   return (
@@ -47,16 +56,15 @@ function EastElevator() {
       <div className="bttn-main-cont">
         {floorBttns.map((num, index) => (
           <div className="single-btn-cont" key={index}>
-            <div className={`btn btn-${buttonStates[index]}`}></div>
+            {/* <div className={`btn btn-${buttonStates[index]}`}></div> */}
             <button
               style={{ border: "1px solid black", borderRadius: "50%" }}
-              type="button"
               className={`btn btn-${buttonStates[index]}`}
               onClick={() => handleClick(index, num)}
             >
               {num}
             </button>
-            <div className={`btn btn-${buttonStates[index]}`}></div>
+            {/* <div className={`btn btn-${buttonStates[index]}`}></div> */}
           </div>
         ))}
       </div>
---
+++ my-app/src/components/EastBuilding/Elavators/Elavator1.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+function Elevator1() {
+  return (
+    <div className="elevator1-main-cont">
+      <h1>Im an elevator</h1>
+    </div>
+  );
+}
